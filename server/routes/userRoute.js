7afec33a5ebfc967e8c8fd08fd1f58883c5db291
Kevin <kevinator900@gmail.com>
2022-03-23 12:44:05 -0700

    fixed conflict

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,14 +1,8 @@
 const router = require("express").Router();
 
-<<<<<<< HEAD
-const {} = require("../controllers/userCon");
-
-module.exports = router;
-=======
 const { postUserLogin, createUser } = require('../controllers/userCon')
 
 router.route('/signup').post(createUser)
 router.route('/login').post(postUserLogin)
 
 module.exports = router;
->>>>>>> afc034364b927e762a637e3c751f8c072a15266d
