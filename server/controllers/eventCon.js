d2f66f41d42a016f970ef4e910b515fb28bc8aaf
KevinMurray123 <booqweefius123@gmail.com>
2022-03-25 15:22:25 -0700

    hjdehjdfhj

--- server/controllers/eventCon.js
+++ server/controllers/eventCon.js
@@ -2,35 +2,106 @@ const EventModel = require("../models/EventModel");
 const UserModel = require("../models/UserModel");
 
 const addEvent = async (req, res) => {
-  return res.status(200).send("addEvent")
+  try {
+    const {user, desc, date, type, featured, location,} = req.body;
+
+    
+
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error at addEvent")
+  }
 };
 
 const getAllEvents = async (req, res) => {
-  return res.status(200).send("getAllEvents")
+  try {
+    const events = await EventModel.find("")
+
+    if(!events) res.status(403).send("No events found...")
+
+    res.status(200).json(events);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error at getAllEvents")
+  }
 };
 
 const deleteAllEvents = async (req, res) => {
-  return res.status(200).send("deleteAllEvents")
+  try {
+    await EventModel.find("").remove(); //? not sure if this will work but dont have a way of testing rn
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error at deleteAllEvents")
+  }
 };
 
 //! /:id
 
 const getEvent = async (req, res) => {
-  return res.status(200).send("getEvent")
+  try {
+    const { id } = req.params;
+
+    const event = await EventModel.findById(id);
+
+    if(!event) res.status(403).send("event not found");
+
+    res.status(200).json(event);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error at getEvent")
+  }
 };
 
 const deleteEvent = async (req, res) => {
-  return res.status(200).send("deleteEvent")
+  try {
+    const userId = req.user.userId;
+    const {id: eventId} = req.params
+
+    const event = await EventModel.findById(eventId);
+    if (!event) res.status(403).send("event not found");
+
+    const user = await UserModel.findById(userId);
+
+    if (event.user.toString() !== userId) {
+      if (user.role === "student" || user.role === "teacher") {
+        await event.remove();
+        return res.status(200).send("event succesfully removed");
+      } else {
+        return res.status(401).send("Unauthorized");
+      }
+    }
+    await event.remove();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error at deleteEvent")
+  }
 };
 
 const editEvent = async (req, res) => {
-  return res.status(200).send("editEvent")
+  try {
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error at editEvent")
+  }
 }
 
 //! /featured
 
 const getFeaturedEvents = async (req, res) => {
-  return res.status(200).send("getFeaturedEvents")
+  try {
+    const featuredEvents = await EventModel.find({ featured: true });
+
+    if(!featuredEvents) res.status(403).send("No featured events found...");
+
+    res.status(200).json(featuredEvents);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("error at getFeaturedEvents")
+  }
 };
 
 module.exports = {
