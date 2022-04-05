30f66d9158fe308cf45fca62cdd7666c31e966f3
Nysa Clark <nclark549@west-mec.local>
2022-04-04 18:29:18 -0700

    commit

--- pages/components/layout/Footer.jsx
+++ pages/components/layout/Footer.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-<<<<<<< HEAD
         <div className='footer'>
-=======
-        <div className="footer">
->>>>>>> a1a9b7474f25fdc0f21a6d4776d867f30719ba85
             © 2022 All Rights Reserved.
         </div>
     )
