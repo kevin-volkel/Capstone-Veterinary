0beacfa15fe77e3d34509abbfb9a6d150ea893aa
Alisson Corrales <Acorra435@west-mec.local>
2022-04-18 18:28:39 -0700

    styling home for multiple screens

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,24 +2,47 @@ import Image from "next/image";
 import { Button, Segment } from "semantic-ui-react";
 import Footer from "./components/layout/Footer";
 import adopt from "../public/media/adoption.png";
+import eventImg from "../public/media/event.png";
 import fEvents from "../public/media/CAT.png";
 import EventSlideshow from "./components/events/EventSlideshow";
 //import "../styles/home.css";
 // import bannerPic from "../public/media/home-page-banner.jpg";
 
-
 export default function Home() {
   return (
     <div className="everything">
       <div className="slideshow">
-          <EventSlideshow />
+        <EventSlideshow />
       </div>
 
       <div className="nf-div">
         <Segment className="events-section">
           <h1 className="nf-title">Featured Events</h1>
           <div className="nf-wholeSect">
-            
+          <div className="nf-sect">
+            <Image
+              src={eventImg}
+              position="relative"
+              className="adopt-image"
+              objectFit="contain"
+            />
+            <p>
+              At vero eos et accus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quosi
+              dolores et quas molestias excepturi sint occaecati cupidi non. At
+              vero eos et accus et iusto odio dignissimos ducimus qui blanditiis
+              At vero eos et accus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quosi
+              dolores et quas molestias excepturi sint occaecati cupidi non. At
+              vero eos et accus et iusto odio dignissimos ducimus qui blanditiis
+            </p>
+          </div>
+          <Button
+            color="white"
+            content="Check Out Events"
+            className="nf-event-btn"
+            onClick=""
+          />
           </div>
         </Segment>
       </div>
@@ -28,13 +51,13 @@ export default function Home() {
         <Segment className="adopt-section">
           <h1 className="nf-title">Find a New Friend!</h1>
           <div className="nf-wholeSect">
-            <Image
-              src={adopt}
-              position="relative"
-              className="adopt-image"
-              objectFit="contain"
-            />
             <div className="nf-sect">
+              <Image
+                src={adopt}
+                position="relative"
+                className="adopt-image"
+                objectFit="contain"
+              />
               <p>
                 At vero eos et accus et iusto odio dignissimos ducimus qui
                 blanditiis praesentium voluptatum deleniti atque corrupti quosi
@@ -46,14 +69,13 @@ export default function Home() {
                 occaecati cupidi non. At vero eos et accus et iusto odio
                 dignissimos ducimus qui blanditiis
               </p>
-
-              <Button
-                color="orange"
-                content="Ready To Adopt"
-                className="nf-adopt-btn"
-                onClick=""
-              />
             </div>
+            <Button
+              color="orange"
+              content="Ready To Adopt"
+              className="nf-adopt-btn"
+              onClick=""
+            />
           </div>
         </Segment>
       </div>
