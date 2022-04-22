b8be294229e4102e7a06ede2a605a06b94051d11
jhunt646 <jhunt646@west-mec.org>
2022-04-22 15:16:32 -0700

    uhhhh stuff maybe things i dont quite know

--- pages/util/dateFuncs.js
+++ pages/util/dateFuncs.js
@@ -1,10 +1,12 @@
 export const convertDate = (date) => {
-  const dateObj = new Date(date)
-  return `${dateObj.getMonth() + 1}/${dateObj.getDate()}/${dateObj.getFullYear()}`
-}
+  const dateObj = new Date(date);
+  return `${
+    dateObj.getMonth() + 1
+  }/${dateObj.getDate()}/${dateObj.getFullYear()}`;
+};
 
 export const sortDates = (a, b) => {
   const aSec = new Date(a.date).getTime();
   const bSec = new Date(b.date).getTime();
-  return bSec - aSec;
-}
+  return aSec - bSec;
+};
