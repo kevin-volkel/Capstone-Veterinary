1406eb529d23e9f3acf0fa0e547a906442ecacb6
Alisson <alisson.corrales24@gmail>
2022-04-21 18:29:38 -0700

    more styling

--- pages/[animal].jsx
+++ pages/[animal].jsx
@@ -2,23 +2,27 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { baseURL } from "./util/auth";
 import { useRouter } from "next/router";
+// import puppy from '../public/media/puppy.png';
 
-const Animal = ({ animalObj }) => {
+// const Animal = ({ animalObj }) => {
+const Animal = () => {
   const router = useRouter();
   const { animalObj } = router.query;
+  const puppy = './media/puppy.png'
 
   useEffect(() => {});
 
   return (
     <div className="page-wrap">
       <div className="animal-wrap">
-        <h2>Animal Name</h2>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et culpa
-          atque numquam quisquam labore ipsum, repellendus molestiae dolores a
-          aperiam beatae fugit laborum laboriosam, ex ipsam reprehenderit libero
-          aut asperiores?
-        </p>
+        <h2 className="animal-name">Animal Name</h2>
+        <img src={puppy} alt="broken doggy :(" />
+        <div className="para-descript">
+          <p>location</p>
+          <p>type</p>
+          <p>gender</p>
+          <p>breed</p>
+        </div>
       </div>
     </div>
   );
