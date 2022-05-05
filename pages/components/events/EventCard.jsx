c93271c7a5bd1a5614110e3991ef893d613a7d90
Kevin <kevinator900@gmail.com>
2022-05-05 15:25:27 -0700

    added more to event modal

--- pages/components/events/EventCard.jsx
+++ pages/components/events/EventCard.jsx
@@ -10,9 +10,7 @@ const EventCard = ({ event, setEventModalShowing }) => {
         src={event.bannerPic}
         alt={event.title}
       />
-      <div
-        className="gradient"
-      />
+      <div className="gradient" />
       <div className="text">
         <h1 className="card-date">{convertDate(event.date)}</h1>
         <div className="card-title">{event.title.toUpperCase()}</div>
