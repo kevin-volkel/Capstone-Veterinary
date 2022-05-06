3f2e8248a1aadf752a8bb14104935a31bbb77820
jhunt646 <jhunt646@west-mec.org>
2022-05-06 15:23:13 -0700

    aria-labels maybe and adobt animal stuff.

--- pages/components/animals/AnimalCard.jsx
+++ pages/components/animals/AnimalCard.jsx
@@ -1,40 +1,35 @@
-import React from "react";
-import { Card, Image, Button, Icon } from "semantic-ui-react";
-import Router from "next/router";
-import { baseURL } from "../../util/auth";
+import React from 'react';
+import { Card, Image, Button, Icon } from 'semantic-ui-react';
+import Router from 'next/router';
+import { baseURL } from '../../util/auth';
 
 const AnimalCard = ({ name, age, type, picURLs, id, gender }) => {
-
-  const titleColor = (gender === 'male') ? '#5797e6' : '#f06687';
-  const genderColor = (gender === 'male') ? '#3777c6' : '#ce4465';
+  const titleColor = gender === 'male' ? '#5797e6' : '#f06687';
+  const genderColor = gender === 'male' ? '#3777c6' : '#ce4465';
 
   return (
     <Card
-      className="animal-card"
-      as="a"
+      className='animal-card'
+      as='a'
       href={`/${id}`}
       // onClick={() => Router.push(`${baseURL}/${id}`)}
     >
-      <Image
-        src={picURLs[0]}
-        alt={`${name} the ${type}`}
-        ariaLabel={`${name} the ${type}`}
-      />
+      <Image src={picURLs[0]} alt={`${name} the ${type}`} />
       <Card.Content>
         <Card.Header
-          className="animal-header"
+          className='animal-header'
           style={{ color: titleColor }}
           content={name}
         />
-        <Card.Meta className="card-type" content={type} />
-        <Card.Meta className="card-gender" style={{ color: genderColor }}>
+        <Card.Meta className='card-type' content={type} />
+        <Card.Meta className='card-gender' style={{ color: genderColor }}>
           {gender}
           <Icon
-            name={gender === "male" ? "man" : "woman"}
-            color={gender === "male" ? "blue" : "pink"}
+            name={gender === 'male' ? 'man' : 'woman'}
+            color={gender === 'male' ? 'blue' : 'pink'}
           />
         </Card.Meta>
-        <Card.Meta className="card-age" content={age} />
+        <Card.Meta className='card-age' content={age} />
       </Card.Content>
     </Card>
   );
--- server/controllers/emailCon.js
+++ server/controllers/emailCon.js
@@ -1,15 +1,15 @@
 const nodemailer = require('nodemailer');
-const UserModel = require('../models/UserModel')
-const emailReg = /^[a-z0-9](\.?[a-z0-9]){3,}@west-mec\.(edu|org)$/gi
+const UserModel = require('../models/UserModel');
+const emailReg = /^[a-z0-9](\.?[a-z0-9]){3,}@west-mec\.(edu|org)$/gi;
 
 require('dotenv').config();
 
 const resetPassword = async (req, res) => {
   try {
-    const {email, code} = req.body;
+    const { email, code } = req.body;
 
-    const user = await UserModel.findOne({email: email.toLowerCase()});
-    if(!user) return res.status(400).send('No user found with that email')
+    const user = await UserModel.findOne({ email: email.toLowerCase() });
+    if (!user) return res.status(400).send('No user found with that email');
 
     const transporter = nodemailer.createTransport({
       service: 'gmail',
@@ -18,13 +18,13 @@ const resetPassword = async (req, res) => {
         pass: process.env.NODEMAILERPASS,
       },
     });
-    
+
     const mailOptions = {
       from: 'sendtest06@gmail.com',
       to: email,
       text: 'Your code to reset your password it ' + code,
     };
-    
+
     transporter.sendMail(
       { ...mailOptions, subject: `Password Reset` },
       function (error, info) {
@@ -35,13 +35,51 @@ const resetPassword = async (req, res) => {
         }
       }
     );
-    console.log('EMAIL SENT')
-    res.status(200).send('Email sent')
+    console.log('EMAIL SENT');
+    res.status(200).send('Email sent');
   } catch (err) {
-    console.log(err)
-    res.status(500).send('error @ resetPassword')
+    console.log(err);
+    res.status(500).send('error @ resetPassword');
   }
-}
+};
 
+const adoptedEmail = async (req, res) => {
+  try {
+    const { userId, animalObj } = req.body;
 
-module.exports = {resetPassword}
+    const user = await UserModel.findOneById(userId);
+    if (!user) return res.status(400).send('No user found with that id');
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'sendtest06@gmail.com',
+        pass: process.env.NODEMAILERPASS,
+      },
+    });
+
+    const mailOptions = {
+      from: 'sendtest06@gmail.com',
+      to: email,
+      text: `Someone is interested in ${animalObj.name}.`,
+    };
+
+    transporter.sendMail(
+      { ...mailOptions, subject: `Someone is interested in adopting!` },
+      function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      }
+    );
+    console.log('EMAIL SENT');
+    res.status(200).send('Email sent');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('error @ resetPassword');
+  }
+};
+
+module.exports = { resetPassword };
